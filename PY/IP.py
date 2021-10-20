39a4272df3fb747394d8e164ddd7111e9cfdd46a
garsemar <martigarciaserra@gmail.com>
2021-10-20 18:54:36 +0200

    IP geo

--- PY/IP.py
+++ PY/IP.py
@@ -1,23 +1,48 @@
-import json
 import requests
 from requests.structures import CaseInsensitiveDict
+import webview
+from tkinter import *
 
 
-# https://stackoverflow.com/questions/40827356/find-a-value-in-json-using-python
-# https://myprojects.geoapify.com/api/HlB7k9Z7VEAv61XxeoWz/keys
-def jprint(obj):
-    # create a formatted string of the Python JSON object
-    text = json.dumps(obj, sort_keys=True, indent=4)
-    for i in text:
-        print(i['state'])
-    print(text)
+def geo(resp_dic=None, latitude=None, longitude=None):
+    webview.create_window(resp_dic['ip'],
+                          f'https://api.maptiler.com/maps/basic/?key=sTjsjRs9F3j7zqgA2aI6#12/{latitude}/{longitude}')
+    webview.start()
 
 
-url = "https://api.geoapify.com/v1/ipinfo?&apiKey=4e9a19bda1f94bed9b57f7ff04b82a6c"
+def main():
+    ip = "https://api.geoapify.com/v1/ipinfo?&apiKey=4e9a19bda1f94bed9b57f7ff04b82a6c"
 
-headers = CaseInsensitiveDict()
-headers["Accept"] = "application/json"
+    headers = CaseInsensitiveDict()
+    headers["Accept"] = "application/json"
 
-resp = requests.get(url, headers=headers)
+    resp = requests.get(ip, headers=headers)
+    resp_dic = resp.json()
+    location = resp_dic['location']
 
-jprint(resp.json())
+    print(resp_dic['ip'])
+    print(location['latitude'])
+    print(location['longitude'])
+
+    window = Tk()
+    window.title("App")
+    icon = PhotoImage(file='logo.png')
+    window.iconphoto(True, icon)
+
+    titLabel = Label(window, text="App", font=("Arial", 15))
+    titLabel.place(relx=0.5, rely=0.0, anchor=N)
+
+    latitude = location["latitude"]
+    longitude = location["longitude"]
+
+    butt = Button(window, text="Hello", command=None, height="2", width="8")
+    butt.place(relx=0.0, rely=1.0, anchor=SW)
+
+    butt = Button(window, text="Location", command=lambda: geo(resp_dic, latitude, longitude), height="2", width="8")
+    butt.place(relx=1.0, rely=1.0, anchor=SE)
+
+    window.mainloop()
+
+
+if __name__ == '__main__':
+    main()
