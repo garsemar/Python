934901699672020d39a78085676c8620473dd3fc
garsemar <martigarciaserra@gmail.com>
2022-02-15 12:36:00 +0100

    Catala words game

---
+++ CatGame/CatGame.py
@@ -0,0 +1,23 @@
+master = "L"
+match = ['E', master, 'S', 'I', 'R', 'T', 'M']
+isV = True
+out = []
+
+f = open("/home/garsemar/PycharmProjects/Python/CatGame/cat.dic", "r")
+for line in f:
+    line = line[:len(line) - 1]
+    line = line.upper()
+    if master in line:
+        for s in line:
+            if s not in match:
+                isV = False
+                break
+            if s in match:
+                isV = True
+        if isV:
+            print(f"{line} is valid")
+            out.append(line)
+        elif isV == False:
+            print(f"{line} no valid")
+
+print(out)
