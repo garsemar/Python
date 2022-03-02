d7cc37ac7ca37714b0f4c23bca713ed4a48e080f
garsemar <martigarciaserra@gmail.com>
2022-03-02 17:35:44 +0100

    Mouse

--- PY/mouse.py
+++ PY/mouse.py
@@ -1,14 +1,21 @@
+import ctypes
+import sys
+from time import sleep
 from ctypes import *
-import ctypes, sys
+
+
 def is_admin():
     try:
         return ctypes.windll.shell32.IsUserAnAdmin()
     except:
         return False
+
+
 if is_admin():
     # Code of your program here
-    while(True):
-        windll.user32.BlockInput(True)
+    windll.user32.BlockInput(True)
+    sleep(10)
+    windll.user32.BlockInput(False)
 else:
     # Re-run the program with admin rights
     ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
