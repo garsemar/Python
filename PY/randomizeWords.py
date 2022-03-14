b8d3af70f6d5b45b0bcc4bfd586056e608b75591
garsemar <martigarciaserra@gmail.com>
2022-03-14 19:32:03 +0100

    Randomize words

--- PY/randomizeWords.py
+++ PY/randomizeWords.py
@@ -1,30 +1,44 @@
-#!/bin/python3
-
-import math
-import os
 from random import *
-import re
-import sys
 
 
-def generate_random_int_list(lenght):
+def generate_random_int_list(word):
     list_nums = []
-    while len(list_nums) != lenght - 2:
-        r = randrange(1, lenght - 1)
+    pos = 1
+    if word[-1] == "," or word[-1] == ".":
+        pos = 2
+    while len(list_nums) != len(word) - (pos + 1):
+        r = randrange(1, len(word) - pos)
         if r not in list_nums:
             list_nums.append(r)
     return list_nums
 
 
-def randomise_word(word):
-    # word = list(word)
+def change_word_order(word, list_nums):
     rand_word = word[0]
-    list_nums = generate_random_int_list(len(word))
     for j in range(len(list_nums)):
         rand_word += word[list_nums[j]]
+    if word[-1] == "," or word[-1] == ".":
+        rand_word += word[-2]
     rand_word += word[-1]
     return rand_word
 
 
-if __name__ == '__main__':
-    print(randomise_word("campus"))
+def str_words_rand(list_words, randomized):
+    for i in list_words:
+        if len(i) <= 3:
+            randomized += i + " "
+        else:
+            list_nums = generate_random_int_list(i)
+            rand_word = change_word_order(i, list_nums)
+            randomized += rand_word + " "
+    return randomized
+
+
+def randomize_word(words):
+    list_words: list = words.split()
+    print(list_words)
+    randomized = ""
+    return str_words_rand(list_words, randomized)
+
+
+print(randomize_word(input()))
